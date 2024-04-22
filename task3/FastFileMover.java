eb58db15a34db3b6f802888d22d728b4cca72490
Ihor Berezovskyi <Ihor_Berezovskyi@epam.com>
2024-04-22 13:23:25 +0300

    Task3 - Finish Task 3 (FastFileMover)

---
+++ task3/FastFileMover.java
@@ -0,0 +1,10 @@
+package task3;
+
+import java.io.IOException;
+
+@FunctionalInterface
+public interface FastFileMover {
+
+    void moveFile(String sourcePath, String destinationPath) throws IOException;
+
+}
